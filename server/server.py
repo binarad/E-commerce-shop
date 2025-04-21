1eb81a607ba570ec2b2b166439351b5c549e7ee8
binarad <pozhidaev.ruslan@gmail.com>
2025-04-21 17:27:27 +0300

    Added patch, delete endpoints and created Product, ProductUpdate, ProductBase, ProductPublic models

--- server/server.py
+++ server/server.py
@@ -1,16 +1,30 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI, Query, HTTPException
 
 # from pydantic import BaseModel
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
-class Product(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class ProductBase(SQLModel):
     name: str = Field(index=True)
     price: float = Field(index=True)
     imgUrl: str | None = Field(default=None, index=True)
     quantity: int = Field(index=True)
+
+
+class Product(ProductBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class ProductPublic(ProductBase):
+    id: int
+
+
+class ProductUpdate(ProductBase):
+    name: str | None = None
+    price: float | None = None
+    imgUrl: str | None = None
+    quantity: int | None = None
 
 
 sqlite_file_name = "database.db"
@@ -39,34 +53,56 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello world"}
 
 
-@app.post("/products/")
-def create_product(product: Product, session: SessionDep) -> Product:
-    session.add(product)
+# Create Product
+@app.post("/products/", response_model=ProductPublic)
+def create_product(product: ProductBase, session: SessionDep):
+    db_product = Product.model_validate(product)
+    session.add(db_product)
     session.commit()
-    session.refresh(product)
+    session.refresh(db_product)
+    return db_product
 
-    return product
 
-
-@app.get("/products")
+# Get Products List
+@app.get("/products/", response_model=list[ProductPublic])
 def get_products_list(
     session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)] = 100
-) -> list[Product]:
+):
     products = session.exec(select(Product).offset(offset).limit(limit))
     return products
 
 
-# @app.get("/products/")
-# async def get_products(skip: int = 0, limit: int = 10):
-#     return fake_item_db[skip : skip + limit]
+# Get Data For 1 Product?
 
 
-# @app.post("/products/")
-# async def create_product(product: Product):
-#     fake_item_db.append(product)
-#     return product
+# Update Product
+@app.patch("/products/{product_id}", response_model=ProductPublic)
+def update_product(product_id: int, product: ProductUpdate, session: SessionDep):
+    product_db = session.get(Product, product_id)
+    if not product_db:
+        raise HTTPException(status_code=404, detail="Product not found")
+    product_data = product.model_dump(exclude_unset=True)
+    product_db.sqlmodel_update(product_data)
+    session.add(product_db)
+    session.commit()
+    session.refresh(product_db)
+    return product_db
+
+
+# Delete Product
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int, session: SessionDep):
+    product = session.get(Product, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    session.delete(product)
+    session.commit()
+    return product
+
+
+# Delete All Products
