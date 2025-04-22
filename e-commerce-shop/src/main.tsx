b9d53e0df3661eb089fc5c2c8b59f0ff51a9a9ad
binarad <poshidaev.ruslan@gmail.com>
2025-04-22 17:11:36 +0300

    Connected react router to project

--- e-commerce-shop/src/main.tsx
+++ e-commerce-shop/src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Admin from './Admin/Admin.tsx'
+
+function Main() {
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <App />,
+		},
+		{
+			path: '/admin',
+			element: <Admin />,
+		},
+	])
+
+	return <RouterProvider router={router} />
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Main />
+	</StrictMode>
 )
