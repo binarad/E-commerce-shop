42353b86461391dafb50188b7cc4c1f839b304c7
binarad <pozhidaev.ruslan@gmail.com>
2025-04-30 13:12:26 +0300

    simple styles

--- e-commerce-shop/src/Admin/Admin.tsx
+++ e-commerce-shop/src/Admin/Admin.tsx
@@ -1,3 +1,13 @@
+// NOT RESPONSIVE PANEL
+import { useState } from 'react'
+import './admin.css'
 export default function Admin() {
-	return <div>Admin</div>
+	const [data, setData] = useState()
+	return (
+		<div id='admin-main'>
+			<div id='admin-panel'>
+				<p>Admin Panel</p>
+			</div>
+		</div>
+	)
 }
