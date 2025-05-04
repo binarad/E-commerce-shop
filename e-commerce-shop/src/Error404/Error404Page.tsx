e9cd6395db42da9f3693c1d5051f6aa7e6712e21
binarad <pozhidaev.ruslan@gmail.com>
2025-05-04 13:39:05 +0300

    stylized the  return button  to the main screen

--- e-commerce-shop/src/Error404/Error404Page.tsx
+++ e-commerce-shop/src/Error404/Error404Page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router'
-import Error404Img from '/page404.svg'
+import Error404Img from '../assets/page404.svg'
 import './style.css'
+import { Button } from '@mui/material'
 export default function Error404Page() {
 	return (
 		<div id='container'>
 			<img src={Error404Img} />
 			<Link to={'/'} id='home-link'>
-				Go to the main page
+				<Button variant='contained' sx={{ width: '200px', height: '50px' }}>
+					Go to the main page
+				</Button>
 			</Link>
 		</div>
 	)
