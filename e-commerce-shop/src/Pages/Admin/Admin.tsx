6fa6e5a74e90feab494f71635c94088e75ca1b60
binarad <pozhidaev.ruslan@gmail.com>
2025-05-13 16:50:59 +0300

    Configured admin page & set up a basic template for it

--- e-commerce-shop/src/Pages/Admin/Admin.tsx
+++ e-commerce-shop/src/Pages/Admin/Admin.tsx
@@ -1,10 +1,34 @@
 // NOT RESPONSIVE PANEL
+import { Link } from 'react-router'
 import './admin.css'
+import { Checkbox } from '@mui/material'
 export default function Admin() {
 	return (
 		<div id='admin-container'>
 			<div id='admin-panel'>
-				<p>Admin Panel</p>
+				<h1 id=''>Admin Panel</h1>
+				<div id='created-products-list'>
+					Here will be list of created products
+					<div className='created-product'>
+						<p id='product-name'>prod name</p>
+						<b id='product-price'>$1999.99</b>
+						<p id='product-quantity'>x199</p>
+						<span id='checkbox'>
+							<Checkbox size='small' />
+						</span>
+					</div>
+				</div>
+				<div id='buttons'>
+					<Link to={'/create-product'}>
+						<button className='bg-sky-600 text-white hover:bg-sky-700'>
+							Create Product
+						</button>
+					</Link>
+					{/* TODO: create function to delete selected product from localStorage */}
+					<button className='bg-red-700 text-white hover:bg-red-800 '>
+						Delete Product
+					</button>
+				</div>
 			</div>
 		</div>
 	)
