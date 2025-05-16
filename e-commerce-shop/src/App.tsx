254b4c5d1b3e615179a2014227b0fec10ce4f393
binarad <pozhidaev.ruslan@gmail.com>
2025-05-17 02:04:37 +0300

    Product card updated & minor tweaks in App.tsx

--- e-commerce-shop/src/App.tsx
+++ e-commerce-shop/src/App.tsx
@@ -1,58 +1,22 @@
 // import { useState } from 'react'
-import NavBar from './Components/NavBar'
-
-interface ProductDataType {
-	name: string
-	price: number
-	imgUrl: string
-	quantity: number
-}
+import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-	// const [data, setData] = useState<ProductDataType | null>(null)
-
-	const ProductData = {
-		name: '33i22',
-		price: 11.2,
-		imgUrl: 'https/sss.com',
-		quantity: 19,
-	}
-
-	async function fetchData() {
-		const resp = await fetch('http://127.0.0.1:8000/products/')
-		const newData: ProductDataType[] = await resp.json()
-		console.log(newData)
-	}
-
-	async function createProduct() {
-		const resp = await fetch('http://127.0.0.1:8000/products/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(ProductData),
-		})
-
-		const data = await resp.json()
-		console.log(data)
-	}
-
-	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				gap: 10,
-				justifyItems: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<NavBar />
-			<h1>App page</h1>
-			<button onClick={fetchData}>Press</button>
-			<button onClick={createProduct}>Create</button>
-		</div>
-	)
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        justifyItems: "center",
+        alignItems: "center",
+      }}
+    >
+      <NavBar />
+      <HomePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- e-commerce-shop/src/Components/ProductCard.tsx
+++ e-commerce-shop/src/Components/ProductCard.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import CartType from '../Pages/Cart/cartData.type'
-const ProductCard: React.FC<CartType> = ({ name, price, imgUrl, quantity }) => {
-	return (
-		<div className='h-[400px] w-[300px] items-center justify-center flex flex-col m-[10px] p-[2.5px] border border-[#d9d9d9] rounded-lg'>
-			<img
-				src={imgUrl}
-				alt={`${name}-img`}
-				className='h-[250px] w-[250px] aspect-square'
-			/>
+import React from "react";
+import { Button } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
 
-			<div className='m-2 flex flex-col w-[220px] justify-center'>
-				<p>{name}</p>
-				<p>${price}</p>
-				<p>In stock: {quantity}</p>
-			</div>
-		</div>
-	)
+interface CardProps {
+  name: string;
+  price: number;
+  quantity: number;
 }
+const ProductCard: React.FC<CardProps> = (props) => {
+  // TODO: Handle Add to Cart
+  return (
+    <div className="h-[300px] w-[200px] items-center   justify-around flex flex-col m-[10px] p-[2.5px] border border-[#d9d9d9] rounded-lg">
+      <img
+        alt="Here must be product img but not today :("
+        className="h-[150px] w-[150px] aspect-square text-center border border-[#d9d9d9] rounded-lg mt-2"
+      />
 
-export default ProductCard
+      <h1>{props.name}</h1>
+      <div className="flex items-center justify-between w-full p-2">
+        <b>${props.price}</b>
+        <Button variant="contained">
+          <AddShoppingCart />
+        </Button>
+      </div>
+      <p className="text-sm">In stock: {props.quantity}</p>
+    </div>
+  );
+};
+
+export default ProductCard;
