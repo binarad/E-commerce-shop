8deef57ead266506f6501a8b3b288779a1fac630
binarad <pozhidaev.ruslan@gmail.com>
2025-05-17 01:23:13 +0300

    Finally have possibility to create Product with "laptop" type

--- e-commerce-shop/src/Pages/CreateProductPage/CreateProductPage.tsx
+++ e-commerce-shop/src/Pages/CreateProductPage/CreateProductPage.tsx
@@ -1,100 +1,214 @@
-import { useEffect, useState } from 'react'
-import './create_product.css'
+import { useState } from "react";
+import "./create_product.css";
 import {
-	Button,
-	FormControl,
-	InputLabel,
-	MenuItem,
-	Select,
-	SelectChangeEvent,
-	TextField,
-} from '@mui/material'
-import LaptopSpecsComp from './LaptopSpecsComp'
-import SmartphoneSpecsComp from './SmartphoneSpecsComp'
-import TabletSpecsComp from './TabletSpecsComp'
-import TVSpecsComp from './TVSpecsComp'
-
-interface Categories {
-	smartphone: string
-	laptop: string
-	tablet: string
-	tv: string
-}
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import LaptopSpecsComp from "./LaptopSpecsComp";
+import SmartphoneSpecsComp from "./SmartphoneSpecsComp";
+import TabletSpecsComp from "./TabletSpecsComp";
+import TVSpecsComp from "./TVSpecsComp";
 
 export default function CreateProductPage() {
-	const [name, setName] = useState<string>('')
-	const [category, setCategory] = useState<string>('')
-	const [manufacturer, setManufacturer] = useState<string>('')
-	const [price, setPrice] = useState<string>('')
-	const [quantity, setQuantity] = useState<string>('')
-	const [warrantyPeriod, setWarrantyPeriod] = useState<string>('')
-	const [laptopScreenSize, setLaptopScreenSize] = useState<string>('')
-	const [laptopScreenRefreshRate, setlaptopScreenRefreshRate] =
-		useState<string>('')
+  const [name, setName] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [manufacturer, setManufacturer] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [quantity, setQuantity] = useState<string>("");
+  const [warrantyMonths, setWarrantyMonths] = useState<string>("");
+  const [screenSize, setScreenSize] = useState<string>("");
+  const [screenRefreshRate, setScreenRefreshRate] = useState<string>("");
+  const [screenResolution, setScreenResolution] = useState<string>("");
+  const [matrixType, setMatrixType] = useState<string>("");
+  const [cpu, setCpu] = useState<string>("");
+  const [cpuGen, setCpuGen] = useState<string>("");
+  const [cpuFreq, setCpuFreq] = useState<string>("");
+  const [gpu, setGpu] = useState<string>("");
+  const [gpuRamSize, setGpuRamSize] = useState<string>("");
+  const [gpuType, setGpuType] = useState<string>("");
+  const [ramSize, setRamSize] = useState<string>("");
+  const [ramType, setRamType] = useState<string>("");
+  const [ramFreq, setRamFreq] = useState<string>("");
+  const [storageSize, setStorageSize] = useState<string>("");
+  const [storageType, setStorageType] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+  const [weight, setWeight] = useState<string>("");
+  const [size, setSize] = useState<string>("");
+  const [networkAdapters, setNetworkAdapters] = useState<string[]>([]);
+  const [usbPorts, setUsbPorts] = useState<string[]>([]);
+  const [videoPorts, setVideoPorts] = useState<string[]>([]);
+  const [audioPorts, setAudioPorts] = useState<string[]>([]);
+  const [wirelessConnections, setWirelessConnections] = useState<string[]>([]);
+  const [deliverySet, setDeliverySet] = useState<string[]>([]);
+  const [operatingSystem, setOperatingSystem] = useState<string>("");
 
-	// const [categories, setCategories] = useState<Categories | null>(null)
+  const ProductPOST = async () => {
+    try {
+      const req = await fetch("http://127.0.0.1:8000/products/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          manufacturer: manufacturer,
+          price: parseFloat(price),
+          quantity: parseInt(quantity),
+          warranty_months: parseInt(warrantyMonths),
+          type: category,
+          specs: {
+            screen_size: parseFloat(screenSize),
+            screen_refresh_rate_gz: parseFloat(screenRefreshRate),
+            screen_resolution: screenResolution,
+            matrix_type: matrixType,
+            cpu: cpu,
+            cpu_generation: cpuGen,
+            cpu_frequency_ghz: parseFloat(cpuFreq),
+            gpu: gpu,
+            gpu_storage_size_gb: parseFloat(gpuRamSize),
+            gpu_type: gpuType,
+            ram_gb: parseInt(ramSize),
+            ram_type: ramType,
+            ram_frequency_ghz: ramFreq,
+            storage: storageSize,
+            storage_type: storageType,
+            color: color,
+            weight_kg: weight,
+            size: size,
+            network_adapters: networkAdapters,
+            usb_ports: usbPorts,
+            video_ports: videoPorts,
+            audio_ports: audioPorts,
+            wireless_connections: wirelessConnections,
+            delivery_set: deliverySet,
+            os: operatingSystem,
+          },
+        }),
+      });
 
-	// const getProductsList = async () => {
-	// 	const req = await fetch('http://127.0.0.1:8000/products/')
-	// 	const resp = await req.json()
-	// 	console.log(resp)
-	// }
-	// useEffect(() => {
-	// 	const fetchCategories = async () => {
-	// 		const resp = await fetch('http://127.0.0.1:8000/categories/')
-	// 		const getCategories = await resp.json()
-	// 		setCategories(getCategories)
-	// 	}
-	// 	fetchCategories()
-	// }, [])
-	return (
-		<div id='create-product-main'>
-			<h1 id='title'>Create Product</h1>
-			{/* Do i need img? */}
-			<div id='create-product-container' className='gap-2.5'>
-				{/* <img id='product-image' alt='product image' />
-				<Button variant='contained' className='w-[250px]'>
-					<label htmlFor='product-image-input'>Click to upload photo</label>
-				</Button>
-				<input type='file' id='product-image-input' className='hidden' /> */}
-
-				<TextField label='Enter the product name' variant='standard' />
-				<FormControl>
-					<InputLabel id='category-select'>Category</InputLabel>
-					<Select
-						id='category-select'
-						label='Category'
-						variant='standard'
-						value={category}
-						onChange={(event: SelectChangeEvent) => {
-							setCategory(event.target.value as string)
-						}}
-					>
-						<MenuItem value='smartphone'>Smartphone</MenuItem>
-						<MenuItem value='laptop'>Laptop</MenuItem>
-						<MenuItem value='tablet'>Tablet</MenuItem>
-						<MenuItem value='tv'>TV</MenuItem>
-					</Select>
-				</FormControl>
-				<TextField variant='standard' type='text' label='Manufacturer' />
-				<TextField variant='standard' type='number' label='Price $' />
-				<TextField variant='standard' type='number' label='Quantity' />
-				<TextField variant='standard' type='number' label='Warranty period' />
-				<p>Technical specifications</p>
-				<hr />
-				{category == 'smartphone' ? (
-					<SmartphoneSpecsComp />
-				) : category == 'laptop' ? (
-					<LaptopSpecsComp />
-				) : category == 'tablet' ? (
-					<TabletSpecsComp />
-				) : (
-					<TVSpecsComp />
-				)}
-				<Button variant='contained' className='w-[250px]'>
-					Create Product
-				</Button>
-			</div>
-		</div>
-	)
+      const resp = await req.json();
+      console.log(resp);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div id="create-product-main">
+      <h1 id="title">Create Product</h1>
+      {/* Do i need img? */}
+      <div id="create-product-container" className="gap-2.5">
+        <TextField
+          label="Enter the product name"
+          variant="standard"
+          onChange={(e) => {
+            e.preventDefault();
+            setName(e.target.value);
+          }}
+        />
+        <FormControl>
+          <InputLabel id="category-select">Category</InputLabel>
+          <Select
+            id="category-select"
+            label="Category"
+            variant="standard"
+            value={category}
+            defaultValue=""
+            onChange={(event: SelectChangeEvent) => {
+              event.preventDefault();
+              setCategory(event.target.value);
+            }}
+          >
+            <MenuItem value="smartphone">Smartphone</MenuItem>
+            <MenuItem value="laptop">Laptop</MenuItem>
+            <MenuItem value="tablet">Tablet</MenuItem>
+            <MenuItem value="tv">TV</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          variant="standard"
+          type="text"
+          label="Manufacturer"
+          onChange={(e) => {
+            e.preventDefault();
+            setManufacturer(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          label="Price $"
+          onChange={(e) => {
+            e.preventDefault();
+            setPrice(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          label="Quantity"
+          onChange={(e) => {
+            e.preventDefault();
+            setQuantity(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          label="Warranty months"
+          onChange={(e) => {
+            e.preventDefault();
+            setWarrantyMonths(e.target.value);
+          }}
+        />
+        <p>Technical specifications</p>
+        <hr />
+        {category == "smartphone" ? (
+          <div>Smartphone</div>
+        ) : // <SmartphoneSpecsComp /> //Dont forget about this
+        category == "laptop" ? (
+          <LaptopSpecsComp
+            setScreenSize={setScreenSize}
+            setScreenRefreshRate={setScreenRefreshRate}
+            setScreenResolution={setScreenResolution}
+            setMatrixType={setMatrixType}
+            setCpu={setCpu}
+            setCpuGen={setCpuGen}
+            setCpuFreq={setCpuFreq}
+            setGpu={setGpu}
+            setGpuRamSize={setGpuRamSize}
+            setGpuType={setGpuType}
+            setRamSize={setRamSize}
+            setRamType={setRamType}
+            setRamFreq={setRamFreq}
+            setStorageSize={setStorageSize}
+            setStorageType={setStorageType}
+            setColor={setColor}
+            setWeight={setWeight}
+            setSize={setSize}
+            setNetworkAdapters={setNetworkAdapters}
+            setUsbPorts={setUsbPorts}
+            setVideoPorts={setVideoPorts}
+            setAudioPorts={setAudioPorts}
+            setWirelessConnections={setWirelessConnections}
+            setDeliverySet={setDeliverySet}
+            setOperatingSystem={setOperatingSystem}
+          />
+        ) : category == "tablet" ? (
+          <TabletSpecsComp />
+        ) : category == "tv" ? (
+          <TVSpecsComp />
+        ) : (
+          ""
+        )}
+        <Button variant="contained" className="w-[250px]" onClick={ProductPOST}>
+          Create Product
+        </Button>
+      </div>
+    </div>
+  );
 }
--- e-commerce-shop/src/Pages/CreateProductPage/LaptopSpecsComp.tsx
+++ e-commerce-shop/src/Pages/CreateProductPage/LaptopSpecsComp.tsx
@@ -1,54 +1,251 @@
-import { Button, TextField } from '@mui/material'
-import { LaptopSpecs } from '../../productData.type'
+import { TextField } from "@mui/material";
 
-export default function LaptopSpecsComp(/*props: LaptopSpecs*/) {
-	// const convertToArray = () => {
-	// 	const string = 'A,b,c,d,e,f,x,1289x129x300,1xWifi,2x Bluetooth'
+interface LaptopSpecsProps {
+  setScreenSize: React.Dispatch<React.SetStateAction<string>>;
+  setScreenRefreshRate: React.Dispatch<React.SetStateAction<string>>;
+  setScreenResolution: React.Dispatch<React.SetStateAction<string>>;
+  setMatrixType: React.Dispatch<React.SetStateAction<string>>;
+  setCpu: React.Dispatch<React.SetStateAction<string>>;
+  setCpuGen: React.Dispatch<React.SetStateAction<string>>;
+  setCpuFreq: React.Dispatch<React.SetStateAction<string>>;
+  setGpu: React.Dispatch<React.SetStateAction<string>>;
+  setGpuRamSize: React.Dispatch<React.SetStateAction<string>>;
+  setGpuType: React.Dispatch<React.SetStateAction<string>>;
+  setRamSize: React.Dispatch<React.SetStateAction<string>>;
+  setRamType: React.Dispatch<React.SetStateAction<string>>;
+  setRamFreq: React.Dispatch<React.SetStateAction<string>>;
+  setStorageSize: React.Dispatch<React.SetStateAction<string>>;
+  setStorageType: React.Dispatch<React.SetStateAction<string>>;
+  setColor: React.Dispatch<React.SetStateAction<string>>;
+  setWeight: React.Dispatch<React.SetStateAction<string>>;
+  setSize: React.Dispatch<React.SetStateAction<string>>;
+  setNetworkAdapters: React.Dispatch<React.SetStateAction<string[]>>;
+  setUsbPorts: React.Dispatch<React.SetStateAction<string[]>>;
+  setVideoPorts: React.Dispatch<React.SetStateAction<string[]>>;
+  setAudioPorts: React.Dispatch<React.SetStateAction<string[]>>;
+  setWirelessConnections: React.Dispatch<React.SetStateAction<string[]>>;
+  setDeliverySet: React.Dispatch<React.SetStateAction<string[]>>;
+  setOperatingSystem: React.Dispatch<React.SetStateAction<string>>;
+}
 
-	// 	console.log(string)
-
-	// 	const arrayString = string.split(',')
-	// 	console.log(arrayString)
-	// }
-
-	return (
-		<div className='box-border flex flex-col'>
-			<h1 className='text-2xl'>Screen</h1>
-			<TextField type='number' variant='standard' label='Screen size (inch)' />
-			<TextField
-				type='number'
-				variant='standard'
-				label='Screen refresh rate (ghz)'
-			/>
-			<TextField type='number' variant='standard' label='Screen resolution' />
-			<TextField variant='standard' label='Matrix type' />
-			<h1 className='text-2xl'>CPU</h1>
-			<TextField variant='standard' label='CPU' />
-			<TextField variant='standard' label='CPU generation' />
-			<TextField type='number' variant='standard' label='CPU frequency (ghz)' />
-			<h1 className='text-2xl'>GPU</h1>
-			<TextField variant='standard' label='GPU' />
-			<TextField type='number' variant='standard' label='GPU Ram size (gb)' />
-			<TextField variant='standard' label='GPU type' />
-			<h1 className='text-2xl'>RAM</h1>
-			<TextField type='number' variant='standard' label='RAM size (gb)' />
-			<TextField variant='standard' label='RAM type' />
-			<TextField type='number' variant='standard' label='RAM frequency (ghz)' />
-			<h1 className='text-2xl'>Storage</h1>
-			<TextField type='number' variant='standard' label='Storage size (gb)' />
-			<TextField variant='standard' label='Storage type' />
-			<br />
-			<hr />
-			<TextField variant='standard' label='Color' />
-			<TextField type='number' variant='standard' label='Weight (kg)' />
-			<TextField variant='standard' label='Size LxWxH' />
-			<TextField variant='standard' label='Network adapters' />
-			<TextField variant='standard' label='USB ports' />
-			<TextField variant='standard' label='Video ports' />
-			<TextField variant='standard' label='Audio ports' />
-			<TextField variant='standard' label='Wireless connections' />
-			<TextField variant='standard' label='Delivery set' />
-			<TextField variant='standard' label='Operating system' />
-		</div>
-	)
+export default function LaptopSpecsComp(props: LaptopSpecsProps) {
+  return (
+    <div className="box-border flex flex-col">
+      <h1 className="text-2xl">Screen</h1>
+      <TextField
+        type="number"
+        variant="standard"
+        label="Screen size (inch)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setScreenSize(e.target.value);
+        }}
+      />
+      <TextField
+        type="number"
+        variant="standard"
+        label="Screen refresh rate (ghz)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setScreenRefreshRate(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Screen resolution"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setScreenResolution(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Matrix type"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setMatrixType(e.target.value);
+        }}
+      />
+      <h1 className="text-2xl">CPU</h1>
+      <TextField
+        variant="standard"
+        label="CPU"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setCpu(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="CPU generation"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setCpuGen(e.target.value);
+        }}
+      />
+      <TextField
+        type="number"
+        variant="standard"
+        label="CPU frequency (ghz)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setCpuFreq(e.target.value);
+        }}
+      />
+      <h1 className="text-2xl">GPU</h1>
+      <TextField
+        variant="standard"
+        label="GPU"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setGpu(e.target.value);
+        }}
+      />
+      <TextField
+        type="number"
+        variant="standard"
+        label="GPU Ram size (gb)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setGpuRamSize(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="GPU type"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setGpuType(e.target.value);
+        }}
+      />
+      <h1 className="text-2xl">RAM</h1>
+      <TextField
+        type="number"
+        variant="standard"
+        label="RAM size (gb)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setRamSize(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="RAM type"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setRamType(e.target.value);
+        }}
+      />
+      <TextField
+        type="number"
+        variant="standard"
+        label="RAM frequency (ghz)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setRamFreq(e.target.value);
+        }}
+      />
+      <h1 className="text-2xl">Storage</h1>
+      <TextField
+        type="number"
+        variant="standard"
+        label="Storage size (gb)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setStorageSize(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Storage type"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setStorageType(e.target.value);
+        }}
+      />
+      <br />
+      <hr />
+      <TextField
+        variant="standard"
+        label="Color"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setColor(e.target.value);
+        }}
+      />
+      <TextField
+        type="number"
+        variant="standard"
+        label="Weight (kg)"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setWeight(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Size LxWxH"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setSize(e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Network adapters"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setNetworkAdapters(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="USB ports"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setUsbPorts(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Video ports"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setVideoPorts(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Audio ports"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setAudioPorts(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Wireless connections"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setWirelessConnections(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Delivery set"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setDeliverySet(e.target.value.split(", "));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Operating system"
+        onChange={(e) => {
+          e.preventDefault();
+          props.setOperatingSystem(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
