1c47fc7e4ed5d076e9902961c4d753ed0eaf3189
binarad <pozhidaev.ruslan@gmail.com>
2025-05-20 14:44:56 +0300

    Fixed errors with .join()

--- e-commerce-shop/src/Pages/CreateProductPage/CreateProductPage.tsx
+++ e-commerce-shop/src/Pages/CreateProductPage/CreateProductPage.tsx
@@ -1,149 +1,166 @@
-import { useState } from 'react'
-import './create_product.css'
+import { useState } from "react";
+import "./create_product.css";
 import {
-	Button,
-	FormControl,
-	InputLabel,
-	MenuItem,
-	Select,
-	SelectChangeEvent,
-	TextField,
-} from '@mui/material'
-import LaptopSpecsComp from './LaptopSpecsComp'
-import SmartphoneSpecsComp from './SmartphoneSpecsComp'
-import TabletSpecsComp from './TabletSpecsComp'
-import TVSpecsComp from './TVSpecsComp'
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import LaptopSpecsComp from "./LaptopSpecsComp";
+import SmartphoneSpecsComp from "./SmartphoneSpecsComp";
+import TabletSpecsComp from "./TabletSpecsComp";
+import TVSpecsComp from "./TVSpecsComp";
 import {
-	LaptopSpecs,
-	ProductSpecs,
-	SmartphoneSpecs,
-	TabletSpecs,
-	TVSpecs,
-} from '../../productData.type'
+  LaptopSpecs,
+  ProductSpecs,
+  SmartphoneSpecs,
+  TabletSpecs,
+  TVSpecs,
+} from "../../productData.type";
+import { useNavigate } from "react-router";
 
 export default function CreateProductPage() {
-	// const [data, setData] = useState<ProductPublic>({
-	// 	name: '',
-	// 	// todo
-	// })
-	const [name, setName] = useState<string>('')
-	const [category, setCategory] = useState<string>('')
-	const [manufacturer, setManufacturer] = useState<string>('')
-	const [price, setPrice] = useState<string>('')
-	const [quantity, setQuantity] = useState<string>('')
-	const [warrantyMonths, setWarrantyMonths] = useState<string>('')
-	const [specs, setSpecs] = useState<ProductSpecs | null>(null)
+  // const [data, setData] = useState<ProductPublic>({
+  // 	name: '',
+  // 	// todo
+  // })
+  const [name, setName] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [manufacturer, setManufacturer] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [quantity, setQuantity] = useState<string>("");
+  const [warrantyMonths, setWarrantyMonths] = useState<string>("");
+  const [specs, setSpecs] = useState<ProductSpecs | null>(null);
+  const navigate = useNavigate();
 
-	const ProductPOST = async () => {
-		try {
-			const req = await fetch('http://127.0.0.1:8000/products/', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					name,
-					manufacturer,
-					price: parseFloat(price),
-					quantity: parseInt(quantity),
-					warranty_months: parseInt(warrantyMonths),
-					type: category,
-					specs,
-				}),
-			})
+  const ProductPOST = async () => {
+    try {
+      const req = await fetch("http://127.0.0.1:8000/products/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          manufacturer,
+          price: parseFloat(price),
+          quantity: parseInt(quantity),
+          warranty_months: parseInt(warrantyMonths),
+          type: category,
+          specs,
+        }),
+      });
 
-			if (req.ok) {
-				const resp = await req.json()
-				console.log(resp)
-				return true
-			}
-		} catch (error) {
-			console.error(error)
-		}
-	}
-	return (
-		<div id='create-product-main'>
-			<h1 id='title'>Create Product</h1>
-			<div id='create-product-container' className='gap-2.5'>
-				<TextField
-					label='Enter the product name'
-					variant='standard'
-					onChange={e => {
-						setName(e.target.value)
-					}}
-				/>
-				<FormControl>
-					<InputLabel id='category-select'>Category</InputLabel>
-					<Select
-						id='category-select'
-						label='Category'
-						variant='standard'
-						value={category}
-						defaultValue=''
-						onChange={(event: SelectChangeEvent) => {
-							event.preventDefault()
-							setCategory(event.target.value)
-						}}
-					>
-						<MenuItem value='smartphone'>Smartphone</MenuItem>
-						<MenuItem value='laptop'>Laptop</MenuItem>
-						<MenuItem value='tablet'>Tablet</MenuItem>
-						<MenuItem value='tv'>TV</MenuItem>
-					</Select>
-				</FormControl>
-				<TextField
-					variant='standard'
-					type='text'
-					label='Manufacturer'
-					onChange={e => {
-						setManufacturer(e.target.value)
-					}}
-				/>
-				<TextField
-					variant='standard'
-					type='number'
-					label='Price $'
-					onChange={e => {
-						setPrice(e.target.value)
-					}}
-				/>
-				<TextField
-					variant='standard'
-					type='number'
-					label='Quantity'
-					onChange={e => {
-						setQuantity(e.target.value)
-					}}
-				/>
-				<TextField
-					variant='standard'
-					type='number'
-					label='Warranty months'
-					onChange={e => {
-						setWarrantyMonths(e.target.value)
-					}}
-				/>
-				<p>Technical specifications</p>
-				<hr />
-				{category == 'smartphone' ? (
-					<SmartphoneSpecsComp
-						specs={specs as SmartphoneSpecs}
-						setSpecs={setSpecs}
-					/> //Dont forget about this
-				) : category == 'laptop' ? (
-					<LaptopSpecsComp specs={specs as LaptopSpecs} setSpecs={setSpecs} />
-				) : category == 'tablet' ? (
-					<TabletSpecsComp specs={specs as TabletSpecs} setSpecs={setSpecs} />
-				) : category == 'tv' ? (
-					<TVSpecsComp specs={specs as TVSpecs} setSpecs={setSpecs} />
-				) : (
-					''
-				)}
-				<Button variant='contained' className='w-[250px]' onClick={ProductPOST}>
-					Create Product
-				</Button>
-				<h1>MAKE a BUTTON TO RETURN TO THE HOME PAGE</h1>
-			</div>
-		</div>
-	)
+      if (req.ok) {
+        const resp = await req.json();
+        console.log(resp);
+        navigate("/admin");
+      }
+
+      if (!name || !category || !manufacturer || !price || !specs) {
+        alert("Please fill all required fields");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div id="create-product-main">
+      <h1 id="title">Create Product</h1>
+      <div id="create-product-container" className="gap-2.5">
+        <TextField
+          required
+          label="Enter the product name"
+          variant="standard"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <FormControl>
+          <InputLabel id="category-select">Category</InputLabel>
+          <Select
+            required
+            id="category-select"
+            label="Category"
+            variant="standard"
+            value={category}
+            defaultValue=""
+            onChange={(event: SelectChangeEvent) => {
+              event.preventDefault();
+              setCategory(event.target.value);
+            }}
+          >
+            <MenuItem value="smartphone">Smartphone</MenuItem>
+            <MenuItem value="laptop">Laptop</MenuItem>
+            <MenuItem value="tablet">Tablet</MenuItem>
+            <MenuItem value="tv">TV</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          variant="standard"
+          type="text"
+          required
+          label="Manufacturer"
+          onChange={(e) => {
+            setManufacturer(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          required
+          label="Price $"
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          label="Quantity"
+          onChange={(e) => {
+            setQuantity(e.target.value);
+          }}
+        />
+        <TextField
+          variant="standard"
+          type="number"
+          label="Warranty months"
+          onChange={(e) => {
+            setWarrantyMonths(e.target.value);
+          }}
+        />
+        <p>Technical specifications</p>
+        <hr />
+        {category == "smartphone" ? (
+          <SmartphoneSpecsComp
+            specs={specs as SmartphoneSpecs}
+            setSpecs={setSpecs}
+          /> //Dont forget about this
+        ) : category == "laptop" ? (
+          <LaptopSpecsComp specs={specs as LaptopSpecs} setSpecs={setSpecs} />
+        ) : category == "tablet" ? (
+          <TabletSpecsComp specs={specs as TabletSpecs} setSpecs={setSpecs} />
+        ) : category == "tv" ? (
+          <TVSpecsComp specs={specs as TVSpecs} setSpecs={setSpecs} />
+        ) : (
+          ""
+        )}
+        <Button
+          variant="contained"
+          className="w-[250px]"
+          onClick={() => {
+            ProductPOST();
+          }}
+        >
+          Create Product
+        </Button>
+        <h1>MAKE a BUTTON TO RETURN TO THE HOME PAGE</h1>
+      </div>
+    </div>
+  );
 }
--- e-commerce-shop/src/Pages/CreateProductPage/TVSpecsComp.tsx
+++ e-commerce-shop/src/Pages/CreateProductPage/TVSpecsComp.tsx
@@ -1,107 +1,108 @@
-import React from 'react'
-import { ProductSpecs, TVSpecs } from '../../productData.type'
-import { Checkbox, FormControlLabel, TextField } from '@mui/material'
+import React from "react";
+import { ProductSpecs, TVSpecs } from "../../productData.type";
+import { Checkbox, FormControlLabel, TextField } from "@mui/material";
 
 type TvSpecsProps = {
-	specs: TVSpecs | null
-	setSpecs: React.Dispatch<React.SetStateAction<ProductSpecs | null>>
-}
+  specs: TVSpecs | null;
+  setSpecs: React.Dispatch<React.SetStateAction<ProductSpecs | null>>;
+};
 
 export default function TVSpecsComp({ specs, setSpecs }: TvSpecsProps) {
-	const handleChange = <K extends keyof TVSpecs>(key: K, value: TVSpecs[K]) => {
-		setSpecs(prev => ({ ...(prev as TVSpecs), [key]: value }))
-	}
-	return (
-		<div className='flex flex-col gap-3'>
-			<TextField
-				variant='standard'
-				label='Screen Size (inch)'
-				onChange={e => {
-					handleChange('screen_size', Number(e.target.value))
-				}}
-			/>
-			<FormControlLabel
-				className='self-start'
-				label='Smart TV support'
-				labelPlacement='start'
-				control={
-					<Checkbox
-						id='smart-tv-support'
-						className='w-12'
-						value={specs?.smart_tv_support || false}
-						// checked={specs?.smart_tv_support}
-						onChange={e => {
-							handleChange('smart_tv_support', e.target.checked)
-							console.log(e.target.checked)
-						}}
-					/>
-				}
-			/>
-			<TextField
-				variant='standard'
-				label='Smart TV'
-				onChange={e => {
-					handleChange('smart_tv', e.target.value)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='OS'
-				onChange={e => {
-					handleChange('os', e.target.value)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Screen resolution'
-				onChange={e => {
-					handleChange('screen_resolution', e.target.value)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Screen refresh rate GHz'
-				onChange={e => {
-					handleChange('screen_refresh_rate_ghz', Number(e.target.value))
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Ports'
-				onChange={e => {
-					handleChange(
-						'ports',
-						e.target.value.split(', ').map(s => s.trim())
-					)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Wireless connections'
-				onChange={e => {
-					handleChange(
-						'wireless_connections',
-						e.target.value.split(', ').map(s => s.trim())
-					)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Border color'
-				onChange={e => {
-					handleChange('border_color', e.target.value)
-				}}
-			/>
-			<TextField
-				variant='standard'
-				label='Additional specs'
-				onChange={e => {
-					handleChange(
-						'additional_specs',
-						e.target.value.split(', ').map(s => s.trim())
-					)
-				}}
-			/>
-		</div>
-	)
+  const handleChange = <K extends keyof TVSpecs>(key: K, value: TVSpecs[K]) => {
+    setSpecs((prev) => ({ ...(prev as TVSpecs), [key]: value }));
+  };
+  return (
+    <div className="flex flex-col gap-3">
+      <TextField
+        type="number"
+        variant="standard"
+        label="Screen Size (inch)"
+        onChange={(e) => {
+          handleChange("screen_size", Number(e.target.value));
+        }}
+      />
+      <FormControlLabel
+        className="self-start"
+        label="Smart TV support"
+        labelPlacement="start"
+        control={
+          <Checkbox
+            id="smart-tv-support"
+            className="w-12"
+            value={specs?.smart_tv_support || false}
+            // checked={specs?.smart_tv_support}
+            onChange={(e) => {
+              handleChange("smart_tv_support", e.target.checked);
+              console.log(e.target.checked);
+            }}
+          />
+        }
+      />
+      <TextField
+        variant="standard"
+        label="Smart TV"
+        onChange={(e) => {
+          handleChange("smart_tv", e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="OS"
+        onChange={(e) => {
+          handleChange("os", e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Screen resolution (ex: 1920x1080)"
+        onChange={(e) => {
+          handleChange("screen_resolution", e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Screen refresh rate GHz"
+        onChange={(e) => {
+          handleChange("screen_refresh_rate_ghz", Number(e.target.value));
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Ports (comma separated)"
+        onChange={(e) => {
+          handleChange(
+            "ports",
+            e.target.value.split(", ").map((s) => s.trim()),
+          );
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Wireless connections"
+        onChange={(e) => {
+          handleChange(
+            "wireless_connections",
+            e.target.value.split(", ").map((s) => s.trim()),
+          );
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Border color"
+        onChange={(e) => {
+          handleChange("border_color", e.target.value);
+        }}
+      />
+      <TextField
+        variant="standard"
+        label="Additional specs"
+        onChange={(e) => {
+          handleChange(
+            "additional_specs",
+            e.target.value.split(", ").map((s) => s.trim()),
+          );
+        }}
+      />
+    </div>
+  );
 }
